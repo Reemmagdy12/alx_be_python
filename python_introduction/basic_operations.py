c9033d1c8a31400652ecf1278d5caf69f3327708
Reemmagdy12 <110478989+Reemmagdy12@users.noreply.github.com>
2024-09-12 23:54:20 +0300

    Delete python_introduction directory

--- python_introduction/basic_operations.py
+++
@@ -1,8 +0,0 @@
-number1 = 30
-number2 = 20
-sum= number1 + number2
-sub= number1 - number2
-mult= number1 * number2
-print ("Addition of ", number1 , " and ",number2 , " is ",sum)
-print ("Subtraction of ", number1 , " and ",number2 , " is ",sub)
-print ("Multiplication of ", number1 , " and ",number2 , " is ",mult)
